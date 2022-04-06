6c177d48706b75a8264b4c3b753dc4414cc128ad
h-latta <h.latta@yahoo.com>
2022-04-06 14:26:08 -0500

    Added Users App to Settings, Created User Models for Backend

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 # Create your models here.
+
+class UserProfile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Owner(models.Model):
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    address = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=13)
+
+class Vet(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    address = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=13)
+
+class Dog(models.Model):
+    owner = models.ForeignKey(Owner, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    breed = models.CharField(max_length=255)
+    birthday = models.DateField()
+    last_checkup = models.DateField()
+    conditions = models.TextField()
