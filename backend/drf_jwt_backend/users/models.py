060aacfe8249a3cee9872165f468f441040eb43b
h-latta <h.latta@yahoo.com>
2022-04-06 14:31:16 -0500

    Deleted UserProfile Model, Made Migrations to MySQL Database

--- backend/drf_jwt_backend/users/models.py
+++ backend/drf_jwt_backend/users/models.py
@@ -4,11 +4,9 @@ User = get_user_model()
 
 # Create your models here.
 
-class UserProfile(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 class Owner(models.Model):
-    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     address = models.CharField(max_length=255)
     phone_number = models.CharField(max_length=13)
 
