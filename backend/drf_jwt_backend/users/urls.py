9fa4cc0b55d0375032924405205b0e767b811c64
h-latta <h.latta@yahoo.com>
2022-04-07 11:19:32 -0500

    Fixed URL Path for Vet Models

--- backend/drf_jwt_backend/users/urls.py
+++ backend/drf_jwt_backend/users/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('owner/', views.get_owner),
     path('owner_register/', views.owner_register),
     path('owner/<int:pk>/', views.update_or_delete_owner),
-    path('vet/', views.get_vet, views.vet_register),
+    path('vet/', views.get_vet),
     path('vet_register/', views.vet_register),
     path('vet/<int:pk>/', views.update_or_delete_vet),
     path('dog/', views.dog_requests),
