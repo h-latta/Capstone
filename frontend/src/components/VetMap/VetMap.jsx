6f69fdc1d18294ae02dbb871c87cbafd2cbd72b7
h-latta <h.latta@yahoo.com>
2022-04-11 11:12:14 -0500

    Removed Preset Markers from Maps

--- frontend/src/components/VetMap/VetMap.jsx
+++ frontend/src/components/VetMap/VetMap.jsx
@@ -2,39 +2,10 @@ import React, { useState } from 'react';
 import { GoogleMap, InfoWindow, LoadScript, Marker } from "@react-google-maps/api";
 
 const VetMap = () => {
-    const initialMarkers = [
-        {
-            position: {
-                lat: 28.625485,
-                lng: 79.821091
-            },
-            label: { color: "white", text: "P1" },
-            draggable: true
-        },
-        {
-            position: {
-                lat: 28.625293,
-                lng: 79.817926
-            },
-            label: { color: "white", text: "P2" },
-            draggable: false
-        },
-        {
-            position: {
-                lat: 28.625182,
-                lng: 79.81464
-            },
-            label: { color: "white", text: "P3" },
-            draggable: true
-        },
-    ];
-    
-    const [activeInfoWindow, setActiveInfoWindow] = useState("");
-    const [markers, setMarkers] = useState(initialMarkers);
 
     const containerStyle = {
-        width: "100%",
-        height: "400px",
+        width: "40%",
+        height: "25rem",
     }
 
     const center = {
@@ -46,14 +17,6 @@ const VetMap = () => {
         console.log(event.latLng.lat(), event.latLng.lng()) 
     }
 
-    const markerClicked = (marker, index) => {  
-        setActiveInfoWindow(index)
-        console.log(marker, index)
-    }
-
-    const markerDragEnd = (marker, index) => { 
-        console.log(marker, index)
-    }
 
     return (
         <LoadScript googleMapsApiKey='AIzaSyDLSqelJc1QOGlSS1jQq6bOha5JFU2IgjE'>
@@ -62,28 +25,9 @@ const VetMap = () => {
                 center={center} 
                 zoom={15}
                 onClick={mapClicked}
-            >
-                {markers.map((marker, index) => (
-                    <Marker 
-                        key={index} 
-                        position={marker.position}
-                        label={marker.label}
-                        draggable={marker.draggable}
-                        onDragEnd={event => markerDragEnd(marker, index)}
-                        onClick={event => markerClicked(marker, index)} 
-                    >
-                        {
-                            (activeInfoWindow === index)
-                            &&
-                            <InfoWindow position={marker.position}>
-                                <b>{marker.position.lat}, {marker.position.lng}</b>
-                            </InfoWindow>
-                        }  
-                    </Marker>
-                ))}
+            >   
             </GoogleMap>
         </LoadScript>
     );
 };
-
 export default VetMap;
